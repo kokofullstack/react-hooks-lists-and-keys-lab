27b0cd49f286ee9a27cc88541580885dccfc7cf6
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-27 17:21:18 -0600

    Complete Lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,20 @@
-import React from "react";
-import ProjectItem from "./ProjectItem";
+import React from 'react';
+import ProjectItem from './ProjectItem';
 
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list"></div>
+      {projects.map((projects) => (
+        <ProjectItem
+          key={projects.id}
+          name={projects.name}
+          about={projects.about}
+          technologies={projects.technologies}
+        />
+      ))}
     </div>
   );
 }

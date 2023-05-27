5c9f7eb9bb9e9fc88c20f32c5376c492f7ca3054
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-27 17:28:51 -0600

    put inside div

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,15 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list"></div>
-      {projects.map((projects) => (
-        <ProjectItem
-          key={projects.id}
-          name={projects.name}
-          about={projects.about}
-          technologies={projects.technologies}
-        />
-      ))}
+      <div id="project-list">
+        {projects.map((projects) => (
+          <ProjectItem
+            key={projects.id}
+            name={projects.name}
+            about={projects.about}
+            technologies={projects.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
